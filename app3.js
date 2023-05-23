91d31ef8e9f5ab2e7a93d3a10270757341882a47
Sanjay2N <sanjaykcbcs@gmail.com>
2023-05-23 13:23:14 +0530

    new changes 3

---
+++ app3.js
@@ -0,0 +1,10 @@
+// var l3=document.getElementsByClassName("list-group-item");
+
+// for(var i=0;i<l3.length;i++){
+//     l3[i].style.backgroundColor="green";
+// }
+
+var Tag=document.getElementsByTagName("li");
+for(var i=0;i<Tag.length;i++){
+    Tag[i].style.backgroundColor="red";
+}
