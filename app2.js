12b2795a96da736dd45d8264e8941177ccc434ba
Sanjay2N <sanjaykcbcs@gmail.com>
2023-05-23 12:54:10 +0530

    ne changes

---
+++ app2.js
@@ -0,0 +1,6 @@
+var l3=document.getElementsByClassName("list-group-item");
+l3[2].style.backgroundColor = "green";
+console.log(l3);
+for(var i=0;i<l3.length;i++){
+    l3[i].style.fontWeight="bold";
+}
